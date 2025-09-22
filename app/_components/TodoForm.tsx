8b04b99d2400958a797823aa7724f81c8d2d5346
ASHISHPUNDIR12 <ashishpundir1012@gmail.com>
2025-09-22 14:04:25 +0530

    fix error

--- app/_components/TodoForm.tsx
+++ app/_components/TodoForm.tsx
@@ -8,9 +8,10 @@ import { addTodo } from "../_lib/action";
 function TodoForm ()  {
   return (
   <div className="scale-[1.01">
-    <form action={addTodo} className="bg-neutral-600  py-10 px-16 text-lg flex gap-5 flex-col" >
+    <form action={addTodo} className="" >
       <div>
-        <input type="text" name="title" />
+        
+        <input className="border" type="text" name="title" />
         <button className="" type="submit" >add</button>
       </div>
     </form>
--- app/todolist/page.tsx
+++ app/todolist/page.tsx
@@ -1,32 +1,32 @@
 import React from "react";
 import TodoForm from "../_components/TodoForm";
 import Todo from "../models/Todo";
-
-
-
+import { connectDB } from "../_lib/mongodb";
+import { revalidatePath } from "next/cache";
 
 async function getAllTodos() {
-  const todos = await Todo.find() ;
-console.log(todos);
-  return todos
+  await connectDB();
+  const todos = await Todo.find();
+  console.log(todos);
+  return todos;
 }
 
-
-
-async function Page ()  {
-const todos =  await getAllTodos()
+async function Page() {
+  const todos = await getAllTodos();
 
   return (
-   <div>
-    <TodoForm/>
+    <div>
+      <TodoForm />
 
-   <ul> 
-  {todos.map((T)=>(
-  <li key={T.id} >{T.title} - Created on: {T.createdAt.toLocaleDateString()} </li>
-  ))}
-</ul>
-   </div>
+      <ul>
+        {todos.map((T) => (
+          <li key={T.id}>
+            {T.title} - Created on: {T.createdAt.toLocaleDateString()}{" "}
+          </li>
+        ))}
+      </ul>
+    </div>
   );
-};
+}
 
 export default Page;
